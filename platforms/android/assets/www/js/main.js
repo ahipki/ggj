10da3276ece410c271160233ded65c6b45b6c85c
Annie Hip-Ki <annie.hipki@etu.u-bordeaux.fr>
2015-01-24 16:52:47 +0100

    essai musique

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -19,4 +19,14 @@ $(function(){
 		$("#logoScreen").hide();
 		gotoSection("home");
 	});
+
+	$("button[go='dance']").click(function(){
+		/*var audio = {};
+        audio["song1"] = new Audio();
+        audio["song1"].src = "./music/song1.mp3"
+        audio["song1"].addEventListener('load', function () {
+            audio["song1"].play();
+        });*/
+	$("body").append(" <audio src='./music/song1.mp3' autoplay></audio>  ")
+	});
 });
