d31d09b3a8f48cc7094e2362edef98e73224f45e
Annie Hip-Ki <annie.hipki@etu.u-bordeaux.fr>
2015-01-25 17:15:16 +0100

    alea taupes

--- www/js/mole.js
+++ www/js/mole.js
@@ -8,14 +8,16 @@ $(function(){
 		}
 	}
 	function aleaMole(){
-		$("#holes").append("<img class='mole' src='img/mole.png' style='top: 80 px; left: 100 px;'/>");
+		var top = Math.floor((Math.random() * 200)); 
+		var left = Math.floor((Math.random() * 200)); 
+		$("#holes").append("<img class='mole' src='img/mole.png' style='top:"+top+" px; left: "+left+"px;'/>");
 	}
 	function initializeGame(){
 		$("#moles .score span").html("0");
 		$("body").css("background-color", "#9ccf31");
 		$("body").css("cursor", "url('./img/mallet.png'), pointer");
 
-		for (var i = 0; i< 101; i++){
+		for (var i = 0; i< 100; i++){
 			aleaMole();
 		}
 
