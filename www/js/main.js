9fdc8a208eb0ff04717da68186c56be5c6658536
Annie Hip-Ki <annie.hipki@etu.u-bordeaux.fr>
2015-01-25 11:32:58 +0100

    images de fond

--- www/js/main.js
+++ www/js/main.js
@@ -2,6 +2,8 @@ $(function(){
 //hide all divs excepted the logoScreen
 	var divs = $(".content").not(":first"); 
 	divs.hide();
+	var guy = "Jason";
+	var girl = "Annie";
 
 //buttons to navigate between screens
 	var button = $("*[go]").not(":first");  
@@ -19,7 +21,42 @@ $(function(){
 		$("#logoScreen").hide();
 		gotoSection("home");
 	});
+//Credits
+	$("#credits").click(function(){
+		$(this).hide();
+		gotoSection("home");
+	});
 
+//Configurations
+$("#configuration button").click(function(){
+	guy = $("#configuration input:first").val();
+	if (guy.length == 0){
+		guy = $("#configuration input:first").attr("placeholder");
+	}
+	girl = $("#configuration input:last").val();
+	if (girl.length == 0){
+		girl = $("#configuration input:last").attr("placeholder");
+	}
+	var spanguy = $(".guy");
+	var spangirl = $(".girl");
+
+	spanguy.each(function(){
+		$(this).replaceWith(guy);
+	});
+	spangirl.each(function(){
+		$(this).replaceWith(girl);
+	});
+	$("#configuration").hide();
+	gotoSection("home");
+});
+	
+//Game 1 - carrots
+
+//Game 2 - moles
+
+//Game 3 - cards
+
+//Game 4 - dance
 	$("button[go='dance']").click(function(){
 		$("body").append(" <audio src='./music/song1.mp3' autoplay></audio>  ")
 	});
